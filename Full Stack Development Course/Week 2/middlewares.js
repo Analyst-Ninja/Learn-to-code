0751f58df958b7db28c642823554953fdd3c2075
Analyst-Ninja <r.kumar01@hotmail.com>
2024-02-05 13:03:41 +0530

    middlewares comments

--- Full Stack Development Course/Week 2/middlewares.js
+++ Full Stack Development Course/Week 2/middlewares.js
@@ -4,11 +4,20 @@ const port = 3000;
 let numberOfRequest = 0;
 const bodyParser = require("body-parser");
 
+function handleFirstRequest(req, res) {
+  // let counter = req.query.counter;       to get the params from query in the URL
+  //   console.log(req.headers);            to get the params from headers
+  //   let counter = req.headers.counter;
+  let counter = req.body.counter;
+
+  res.send(`Answer for sum from 1 to ${counter} is ${calculateSum(counter)}`);
+}
+
 function middleware1(req, res, next) {
   //   console.log("Inside from middleware", req.headers.counter);
   //   console.log("Inside from middleware", req);
   numberOfRequest += 1;
-  console.log(req.body);
+  //   console.log(req.body);
   if (numberOfRequest < 5) {
     next();
   } else {
@@ -18,18 +27,11 @@ function middleware1(req, res, next) {
   //   res.send("error from inside the middleware");
 }
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // It gives the parsed body to the next middleware in series
 
 app.use(middleware1);
-
-function handleFirstRequest(req, res) {
-  // let counter = req.query.counter;       to get the params from query in the URL
-  //   console.log(req.headers);            to get the params from headers
-  //   let counter = req.headers.counter;
-  let counter = req.body.counter;
-
-  res.send(`Answer for sum from 1 to ${counter} is ${calculateSum(counter)}`);
-}
+// It check the request and pass on the control to the nex in series --
+// It can do the various things like - Authentication, Checking the Number of requests etc
 
 app.get("/handleSum", handleFirstRequest);
 
